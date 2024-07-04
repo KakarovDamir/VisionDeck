3cbf4480ba6e78adc1aab7f81a62bf5fdfdcce0e
Damir <kdamir2004@gmail.com>
2024-07-05 03:45:43 +0500

    deploy

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               <div className="flex flex-col gap-2 sm:flex-row">
                 <Link href="/pptx">
                   <button className="px-8 py-2.5 bg-blue-700 text-white font-semibold rounded-md shadow-md hover:bg-blue-500 hover:scale-105 transition duration-300 ease-in-out text-xl">
-                    Dive In ->
+                    Dive In 
                   </button>
                 </Link>
               </div>
